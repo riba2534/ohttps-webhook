86d8550cc688e2cbfe8869c1871ef451546bf34b
riba2534 <riba2534@qq.com>
2023-05-24 14:25:07 +0800

    优化构建

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 set -ex
 
 
+CGO_ENABLED=0 GOOS=linux go build -o main .
 docker build -t ohttps-webhook .
 docker tag ohttps-webhook riba2534/ohttps-webhook
 docker push riba2534/ohttps-webhook
--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
 module github.com/riba2534/ohttps-webhook
 
-go 1.18
+go 1.19
 
 require github.com/gin-gonic/gin v1.9.0
 
